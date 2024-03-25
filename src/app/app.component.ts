39c890d768c198c8fcfd0885ad8dac203f069f6f
Chia Che Chang <midtic0404@gmail.com>
2024-03-25 12:50:31 -0600

    Defer examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChildOneComponent } from './child-one/child-one/child-one.component';
+import { ChildTwoComponent } from './child-two/child-two/child-two.component';
+import { ChildThreeComponent } from './child-three/child-three/child-three.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChildOneComponent, ChildTwoComponent, ChildThreeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'defer-demo';
+  count = 0;
+  example = 'timer';
+
+  increment() {
+    this.count++;
+  }
+
+  changeExample(value: string) {
+    this.example = value;
+}
 }
